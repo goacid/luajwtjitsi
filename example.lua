e2834d80a10db8645b80034a4769eeba55448835
LunaCI <lunaci@luadist.org>
2016-08-03 15:31:22 +0200

    Update to version 1.3-5

---
+++ example.lua
@@ -0,0 +1,36 @@
+#!/usr/bin/env lua
+
+local function t2s(o)
+        if type(o) == 'table' then
+                local s = '{ '
+                for k,v in pairs(o) do
+                        if type(k) ~= 'number' then k = '"'..k..'"' end
+                        s = s .. '['..k..'] = ' .. t2s(v) .. ','
+                end
+
+                return s .. '} '
+        else
+                return tostring(o)
+        end
+end
+
+-- 
+local jwt = require "luajwt"
+
+local key = "example_key"
+
+local claim = {
+	iss = "12345678",
+	nbf = os.time(),
+	exp = os.time() + 3600,
+}
+
+local alg = "HS256" -- default alg
+local token, err = jwt.encode(claim, key, alg)
+
+print("Token:", token)
+
+local validate = true -- validate exp and nbf (default: true)
+local decoded, err = jwt.decode(token, key, validate)
+
+print("Claim:", t2s(decoded) )
